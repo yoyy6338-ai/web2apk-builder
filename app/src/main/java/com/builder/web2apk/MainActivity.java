fab44db483d280619ec0329f0520d635e7ef26a6
yoyy6338-ai <yoyy6338@gmail.com>
2026-06-19 19:11:17 +0700

    Update MainActivity.java

--- app/src/main/java/com/builder/web2apk/MainActivity.java
+++ app/src/main/java/com/builder/web2apk/MainActivity.java
@@ -1,1 +1,22 @@
+package com.builder.web2apk;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://google.com");
+    }
+}
